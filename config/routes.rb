ed876481d0a643e960127004a14929ef3c8511a7
gacurl <greg.a.curl@gmail.com>
2023-08-06 09:59:05 -0400

    add bootstrap and update home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/index'
+
+  root to: 'home#index'
   devise_for :users
-  root 'days#index', as: 'home'
+  # root 'days#index', as: 'home'
 
   get 'search', to: 'search#index'
 
