2f81075989c3eccf01187133e48f894178f811b7
gacurl <greg.a.curl@gmail.com>
2023-08-12 13:22:01 -0400

    add signed out functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
   root to: 'home#index'
+  
+  devise_scope :user do
+    get "/sign_in" => "devise/sessions#new"
+  end
   devise_for :users
   # root 'days#index', as: 'home'
 
