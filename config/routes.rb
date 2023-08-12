cb212918d2527737697057a8b54e05403ca66d29
gacurl <greg.a.curl@gmail.com>
2023-08-12 13:34:45 -0400

    authenticate user before days#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
 
-  root to: 'home#index'
-  
-  devise_scope :user do
-    get "/sign_in" => "devise/sessions#new"
-  end
-  devise_for :users
-  # root 'days#index', as: 'home'
-
-  get 'search', to: 'search#index'
-
   resources :days do
     resources :notes
   end
+  
+  devise_for :users
+  root 'days#index'
+  devise_scope :user do
+    get "/sign_in" => "devise/sessions#new"
+  end
+
+
 
 end
