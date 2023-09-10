21388ae9ddd9c857d2b06974a242b6069704e052
gacurl <greg.a.curl@gmail.com>
2023-09-10 11:09:41 -0400

    update error messages for day model validations

--- app/models/day.rb
+++ app/models/day.rb
@@ -2,15 +2,16 @@ class Day < ApplicationRecord
     belongs_to :user
     has_many :notes
     
-    validates :mooddate, uniqueness: true
-    validates :moodword, :moodjournal, 
-                presence: true , allow_blank: false
-    validates :moodrating, presence: true, 
+    validates :mooddate, uniqueness: true, presence: { message: '- You must select a date.' }
+    validates :moodjournal, presence: { message: '- Please add some notes for your future self.' }
+    validates :moodword, presence: { message: '- Please add a single word describing your day.' }
+    validates :moodrating, 
                 numericality: { 
                     only_integer: true, 
                     less_than_or_equal_to: 10, 
                     greater_than_or_equal_to: 0
-                } 
+                },
+                presence: { message: '- Please select a value.' }
 
     def self.ransackable_attributes(auth_object = nil)
         ["moodrating", "moodword", "moodjournal", "mooddate", "isbookmarked"]
