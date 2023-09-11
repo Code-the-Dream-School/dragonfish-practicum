ece2ebcfb4da3d3de2f4e71f9e555fed35eac8ae
gacurl <greg.a.curl@gmail.com>
2023-09-11 12:35:58 -0400

    changed db to postgresql from heroku deployment

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby "3.1.2"
 gem "rails", "~> 7.0.4", ">= 7.0.4.2"
 gem "sprockets-rails"
-gem "sqlite3", "~> 1.4"
+gem "pg"
 gem "puma", "~> 5.0"
 gem "importmap-rails"
 gem "turbo-rails"
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_09_09_214504) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "days", force: :cascade do |t|
     t.date "mooddate"
     t.integer "moodrating"
@@ -19,7 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_09_214504) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "isbookmarked"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_days_on_user_id"
   end
 
